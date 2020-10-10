8eb264323b5321502fe46641d4d828145405c79e
boi123212321 <46821810+boi123212321@users.noreply.github.com>
2020-10-10 18:45:01 +0200

    adjust loader

--- download.ts
+++ download.ts
@@ -1,6 +1,11 @@
 import { createWriteStream, existsSync } from "fs";
 import Axios from "axios";
 import { SingleBar, Presets } from "cli-progress";
+import { cyan } from "chalk";
+
+function downloadSpeed(bytes: number, secs: number) {
+  return bytes / secs;
+}
 
 export async function downloadFile(url: string, file: string) {
   if (existsSync(file)) {
@@ -10,14 +15,28 @@ export async function downloadFile(url: string, file: string) {
 
   console.error(`\tDownloading ${url} to ${file}...`);
 
-  const downloadBar = new SingleBar({}, Presets.legacy);
-  downloadBar.start(100, 0);
+  const downloadBar = new SingleBar(
+    {
+      format: `Fetching |${cyan(
+        "{bar}"
+      )}| {percentage}% | {loaded}/{totalSize}MB | Speed: {speed}kB/s`,
+    },
+    Presets.shades_classic
+  );
+  downloadBar.start(100, 0, {
+    percentage: "0",
+    loaded: 0,
+    totalSize: 0,
+    speed: "N/A",
+  });
 
   const response = await Axios({
     url: url,
     method: "GET",
     responseType: "stream",
   });
+
+  const start = +new Date();
 
   const writer = createWriteStream(file);
 
@@ -27,14 +46,23 @@ export async function downloadFile(url: string, file: string) {
   response.data.on("data", (data: Buffer) => {
     loaded += Buffer.byteLength(data);
     const percent = ((loaded / totalSize) * 100).toFixed(0);
-    downloadBar.update(+percent);
+    const bytesPerSec = downloadSpeed(loaded, (Date.now() - start) / 1000);
+    downloadBar.update(parseInt(percent), {
+      percentage: percent,
+      loaded: (loaded / 1000 / 1000).toFixed(2),
+      totalSize: (totalSize / 1000 / 1000).toFixed(2),
+      speed: Math.round(bytesPerSec / 1000),
+    });
   });
 
   response.data.pipe(writer);
 
   await new Promise((resolve, reject) => {
     writer.on("finish", resolve);
-    writer.on("error", reject);
+    writer.on("error", (err) => {
+      downloadBar.stop();
+      reject();
+    });
   });
 
   downloadBar.stop();
